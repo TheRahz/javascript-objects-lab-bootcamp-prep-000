e10c61ef018aec5fbb15edcec72ef292a1a200b7
rolandzagonyi@gmail.com <rolandzagonyi@gmail.com>
2018-08-03 21:36:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-   var object = Object.assign({}, recipes)
    object[key] = value
    return object
 }
